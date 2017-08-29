7a0993faf07cbf723e05c73c420b6c94d81db1c9
Alexey Sarapulov <aesee@mail.ru>
2017-08-29 17:31:33 +0600

    Have done Incapsulation.Weights

---
+++ Incapsulation.Weights.exercise/Indexer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Weights
+{
+    public class Indexer
+    {
+        private int mod;
+        private int length;
+
+        private double[] Array { get; set; }
+
+        public int Length
+        {
+            get { return length; }
+            set
+            {
+                if (value >= 0 && value <= Array.Length) length = value;
+                else
+                    throw new ArgumentException();
+            }
+        }
+
+        public int Mod
+        {
+            get { return mod; }
+            set
+            {
+                if (value >= 0) mod = value;
+                else
+                    throw new ArgumentException();
+            }
+        }
+
+        public Indexer(double[] array, int mod, int length)
+        {
+            Array = array;
+            Mod = mod;
+            Length = length;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= Length) return Array[index + Mod];
+                else
+                    throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index <= Length) Array[index + Mod] = value;
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Incapsulation.Weights.exercise/Indexer_should.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Weights
+{
+    [TestFixture]
+    public class Indexer_should
+    {
+        double[] array = new double[] { 1, 2, 3, 4 };
+
+/*        [Test]
+        public void HaveCorrectLength()
+        {
+            var indexer = new Indexer(array, 1, 2);
+            Assert.AreEqual(2,indexer.Length);
+        } */
+
+        [Test]
+        public void GetCorrectly()
+        {
+            var indexer = new Indexer(array, 1, 8);
+ //           Assert.AreEqual(2, indexer[0]);
+ //           Assert.AreEqual(3, indexer[15]);
+            Assert.AreEqual(3, indexer[1]);
+            Assert.AreEqual(4, indexer[2]);
+            Assert.AreEqual(5, indexer[3]);
+            Assert.AreEqual(3, indexer[4]);
+            Assert.AreEqual(3, indexer[5]);
+            Assert.AreEqual(3, indexer[6]);
+        }
+
+/*       [Test]
+        public void SetCorrectly()
+        {
+            var indexer = new Indexer(array, 1, 2);
+            indexer[0] = 10;
+            Assert.AreEqual(10,array[1]);
+        }
+
+        [Test]
+        public void FailWithWrongArguments1()
+        {
+            Assert.Throws(typeof(ArgumentException), () => new Indexer(array, -1, 3));
+        }
+
+        [Test]
+        public void FailWithWrongArguments2()
+        {
+            Assert.Throws(typeof(ArgumentException), () => new Indexer(array, 1, -1));
+        }
+
+        [Test]
+        public void FailWithWrongArguments3()
+        {
+            Assert.Throws(typeof(ArgumentException), () => new Indexer(array, 1, 10));
+        }
+
+        [Test]
+        public void FailWithWrongIndexing1()
+        {
+            var indexer = new Indexer(array, 1, 2);
+            Assert.Throws(typeof(IndexOutOfRangeException), () => { var a=indexer[-1]; });
+        }
+
+        [Test]
+        public void FailWithWrongIndexing2()
+        {
+            var indexer = new Indexer(array, 1, 2);
+            Assert.Throws(typeof(IndexOutOfRangeException), () => { var a = indexer[10]; });
+        } */
+    }
+}

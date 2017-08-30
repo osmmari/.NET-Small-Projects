005918bcb516ce74ac135f3fd8b2da8667234044
Alexey Sarapulov <aesee@mail.ru>
2017-08-31 02:21:09 +0600

    Simple Pluralize exercise

---
+++ Pluralize.exercise/PluralizeTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+            string result;
+            if ((((count - 1) % 10 == 0) && ((count + 89) % 100 != 0)) || count == 1) result = "рубль";
+            else if ((((count - 2) % 10 == 0) && ((count + 88) % 100 != 0)) || count == 2) result = "рубля";
+            else if ((((count - 3) % 10 == 0) && ((count + 87) % 100 != 0)) || count == 3) result = "рубля";
+            else if ((((count - 4) % 10 == 0) && ((count + 86) % 100 != 0)) || count == 4) result = "рубля";
+            else result = "рублей";
+            return result;
+		}
+	}
+}
---
+++ Pluralize.exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Pluralize
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			// Это пример ввода сложных данных из файла.
+			// Циклы, строки, массивы будут рассмотрены на лекциях чуть позже, но это не должно быть препятствием вашему любопытству! :)
+			string[] lines = File.ReadAllLines("rubles.txt");
+			bool hasErrors = false;
+			foreach (var line in lines)
+			{
+				string[] words = line.Split(' ');
+				int count = int.Parse(words[0]);
+				string rightAnswer = words[1];
+				string pluralizedRubles = PluralizeTask.PluralizeRubles(count);
+				if (pluralizedRubles != rightAnswer)
+				{
+					hasErrors = true;
+					Console.WriteLine(String.Format("Wrong answer: {0} {1}", count, pluralizedRubles));
+				}
+			}
+			if (!hasErrors)
+				Console.WriteLine("Correct!");
+            Console.WriteLine("hi");
+		}
+	}
+}

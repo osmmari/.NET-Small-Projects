d53eade9e900f23dfa59bf838cf1c564c4109dfb
Alexey Sarapulov <aesee@mail.ru>
2017-09-03 22:12:15 +0600

    Mazes Task

---
+++ Mazes.exercise/EmptyMazeTask.cs
@@ -0,0 +1,14 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            for (int i = 0; i < width - 3; i++)
+            robot.MoveTo(Direction.Right);
+            
+            for (int i = 0; i < height - 3; i++)
+            robot.MoveTo(Direction.Down);
+		}
+	}
+}
---
+++ Mazes.exercise/SnakeMazeTask.cs
@@ -0,0 +1,13 @@
+﻿namespace Mazes
+{
+	public static class SnakeMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            for (int i = 0; i < (height - 1) / 2; i++)
+            {
+
+            }
+		}
+	}
+}

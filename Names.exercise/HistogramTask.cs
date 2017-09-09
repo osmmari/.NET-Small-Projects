092ee967ccdd1e6adff2ec6e80dd459721bd39c0
Alexey Sarapulov <aesee@mail.ru>
2017-09-09 17:53:13 +0600

    HistogramsTask have done

--- Names.exercise/HistogramTask.cs
+++ Names.exercise/HistogramTask.cs
@@ -21,8 +21,23 @@ namespace Names
 			Пример подготовки данных для гистограммы смотри в файле HistogramSample.cs
 			*/
 
-            var days = new string[0];
-            var birthCounts = new double[0];
+            var days = new string[31];
+            var birthCounts = new double[31];
+
+            // X axis (days)
+            for (int i = 1; i <= 31; i++)
+            {
+                days[i - 1] = i.ToString();
+            }
+
+            // Y values (counts of birthes)
+            foreach (var nameSample in names)
+            {
+                if (nameSample.Name == name)
+                {
+                    if (nameSample.BirthDate.Day != 1) birthCounts[nameSample.BirthDate.Day-1]++;
+                }
+            }
 
 			return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), days, birthCounts);
 		}

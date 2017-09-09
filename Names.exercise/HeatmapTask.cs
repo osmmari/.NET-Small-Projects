c83c15d4e02d8c8b5e4fc8a0167eeefb7afa12c0
Alexey Sarapulov <aesee@mail.ru>
2017-09-09 18:07:28 +0600

    HeatmapTask have done

--- Names.exercise/HeatmapTask.cs
+++ Names.exercise/HeatmapTask.cs
@@ -7,7 +7,7 @@ namespace Names
 
 		public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
 		{
-			/*
+            /*
 			Подготовьте данные для построения карты интенсивностей, у которой по оси X — число месяца, по Y — номер месяца, 
 			а интенсивность точки (она отображается цветом и размером) обозначает количество рожденных людей в это число этого месяца.
 			Не учитывайте людей, родившихся первого числа любого месяца.
@@ -19,8 +19,31 @@ namespace Names
 			Таким образом, данные для карты интенсивностей должны быть в виде двумерного массива 30 на 12 —  от 2 до 31 числа и от января до декабря.
 			*/
 
-			return new HeatmapData("Пример карты интенсивностей",
-				new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, new[] { "a", "b", "c", "d" }, new[] { "X", "Y", "Z" });
+            var days = new string[30];
+            var months = new string[12];
+            var values = new double[30,12];
+
+            // X axis (days)
+            for (int i = 2; i <= 31; i++)
+            {
+                days[i - 2] = i.ToString();
+            }
+
+            // Y axis (months)
+            for (int i = 1; i <= 12; i++)
+            {
+                months[i - 1] = i.ToString();
+            }
+
+            foreach (var name in names)
+            {
+                if (name.BirthDate.Day != 1)
+                {
+                    values[name.BirthDate.Day-2, name.BirthDate.Month - 1]++;
+                }
+            }
+
+            return new HeatmapData("Карта интенсивностей", values, days, months);
 		}
 	}
 }

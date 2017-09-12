b1cbba555c63540cd18f2da1c40bb0fef1b06f77
Alexey Sarapulov <aesee@mail.ru>
2017-09-12 22:20:25 +0600

    Not correct solve

--- image.exercise/MedianFilterTask.cs
+++ image.exercise/MedianFilterTask.cs
@@ -2,9 +2,9 @@
 
 namespace Recognizer
 {
-	internal static class MedianFilterTask
-	{
-		/* 
+    internal static class MedianFilterTask
+    {
+        /* 
 		 * Для борьбы с пиксельным шумом, подобным тому, что на изображении,
 		 * обычно применяют медианный фильтр, в котором цвет каждого пикселя, 
 		 * заменяется на медиану всех цветов в некоторой окрестности пикселя.
@@ -13,9 +13,41 @@ namespace Recognizer
 		 * Используйте окно размером 3х3 для не граничных пикселей,
 		 * Окно размером 2х2 для угловых и 3х2 или 2х3 для граничных.
 		 */
-		public static double[,] MedianFilter(double[,] original)
-		{
-			return original;
-		}
-	}
+        public static double[,] MedianFilter(double[,] original)
+        {
+            var resultImage = new double[original.GetLength(0), original.GetLength(1)];
+            for (int x = 0; x < original.GetLength(0); x++)
+                for (int y = 0; y < original.GetLength(1); y++)
+                {
+                    int width = 3;
+                    int heght = 3;
+
+                    if (x == 0) width = 2;
+                    else if (x == original.GetLength(0) - 1) width = -2;
+
+                    if (y == 0) heght = 2;
+                    else if (y == original.GetLength(1) - 1) heght = -2;
+
+                    resultImage[x, y] = Median(original, x, y, width, heght);
+                }
+
+            return resultImage;
+        }
+
+        public static double Median(double[,] original, int x, int y, int windowWidth, int windowHeight)
+        {
+            double result = original[x, y];
+            int count = 1;
+
+            if (windowWidth == 3) { result += original[x - 1, y] + original[x + 1, y]; count += 2; }
+            else if (windowWidth == 2) { result += original[x + 1, y]; count++; }
+            else { result += original[x - 1, y]; count++; }
+
+            if (windowHeight == 3) { result += original[x, y - 1] + original[x, y + 1]; count += 2; }
+            else if (windowHeight == 2) { result += original[x, y + 1]; count++; }
+            else { result += original[x, y - 1]; count++; }
+
+            return result / count;
+        }
+    }
 }
--- image.exercise/ThresholdFilterTask.cs
+++ image.exercise/ThresholdFilterTask.cs
@@ -1,8 +1,11 @@
-﻿namespace Recognizer
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recognizer
 {
-	public static class ThresholdFilterTask
-	{
-		/* 
+    public static class ThresholdFilterTask
+    {
+        /* 
 		 * Замените пиксели ярче порогового значения T на белый (1.0),
 		 * а остальные на черный (0.0).
 		 * Пороговое значение найдите так, чтобы:
@@ -11,9 +14,25 @@
 		 *  - белыми стало как можно меньше пикселей.
 		*/
 
-		public static double[,] ThresholdFilter(double[,] original, double threshold)
-		{
-			return new double[original.GetLength(0), original.GetLength(1)];
-		}
-	}
+        public static double[,] ThresholdFilter(double[,] original, double threshold)
+        {
+            var resultImage = new double[original.GetLength(0), original.GetLength(1)];
+
+            int numberOfPixels = (int)(original.GetLength(0) * original.GetLength(1) * threshold);
+            var allElements = new List<double>();
+            foreach (var element in original)
+            {
+                allElements.Add(element);
+            }
+            allElements.Sort();
+            allElements.Reverse();
+            double T = allElements[numberOfPixels];
+
+            for (int x = 0; x < original.GetLength(0); x++)
+                for (int y = 0; y < original.GetLength(1); y++)
+                    resultImage[x, y] = original[x, y] >= T ? 1.0 : 0.0;
+
+            return resultImage;
+        }
+    }
 }

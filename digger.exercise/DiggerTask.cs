d804fb289685a85a76408a763f64a134cdb8d433
Alexey Sarapulov <aesee@mail.ru>
2017-10-09 03:35:14 +0500

    Nothing serious

--- digger.exercise/DiggerTask.cs
+++ digger.exercise/DiggerTask.cs
@@ -58,6 +58,21 @@ namespace Digger
                 default:
                     break;
             }
+            if (!(x + dX >= 0 && x + dX < Game.MapWidth &&
+                y + dY >= 0 && y + dY < Game.MapHeight))
+            {
+                Console.WriteLine("x = {0}, y = {1}", x, y);
+                Console.WriteLine(x + dX > 0);
+                Console.WriteLine(x + dX < Game.MapWidth);
+                Console.WriteLine(y + dY > 0);
+                Console.WriteLine(y + dY < Game.MapHeight);
+                Console.WriteLine("dx = {0} dy = {1}", dX, dY);
+                Console.WriteLine("Width = {0}, Height = {1}", Game.MapWidth, Game.MapHeight);
+                dX = 0;
+                dY = 0;
+            }
+            if (Game.Map[x + dX, y + dY].ToString().Equals("T"))
+                Console.WriteLine("Its working");
             return new CreatureCommand() { DeltaX = dX, DeltaY = dY };
         }
 

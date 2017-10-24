2ec26c63d1221c538a49ce73c58d079aabe688d4
Alexey Sarapulov <aesee@mail.ru>
2017-10-25 01:24:38 +0600

    Finished hotel task

---
+++ hotelaccounting.exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelAccounting
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var data = new AccountingModel();
+            var wnd = new MainWindow();
+            wnd.DataContext = data;
+            new Application().Run(wnd);
+        }
+    }
+}

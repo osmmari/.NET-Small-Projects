d47f6d0a6847df89cf3a0df402e54cc5af5c6ebe
Alexey Sarapulov <aesee@mail.ru>
2017-10-29 00:26:07 +0600

    Todo: fix the Time limit exceeded error

--- pocketgoogle.exercise/Indexer.cs
+++ pocketgoogle.exercise/Indexer.cs
@@ -9,35 +9,28 @@ namespace PocketGoogle
     public class Indexer : IIndexer
     {
         private Dictionary<int, List<string>> words = new Dictionary<int, List<string>> { };
-        private Dictionary<int, string> strings = new Dictionary<int, string> { };
         private int id;
         private readonly char[] separator = { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
 
         public int Id
         {
             get { return id; }
-            set { id = value;
-                if (!words.ContainsKey(id))
-                    words.Add(id, new List<string>()); }
+            set { id = value;}
         }
 
         public List<string> Words
         {
-            //get { return words; }
-            set { if (words.ContainsKey(Id))
-                    words[Id] = value;
-                  else words.Add(Id, value); }
+            set { if (words.ContainsKey(id))
+                    words[id].AddRange(value);
+                  else words.Add(id, value); }
         }
 
         public void Add(int id, string documentText)
         {
-            //Console.WriteLine("Add");
+            Console.WriteLine("id = {0}, document = {1}", id, documentText);
             Id = id;
-            if (!strings.ContainsKey(id)) strings.Add(id, documentText);
-            else strings[id] = documentText;
             var listOfWords = documentText.Split(separator);
             Words = listOfWords.ToList<string>();
-            //Console.WriteLine("The end of Add");
         }
 
         public List<int> GetIds(string word)
@@ -49,59 +42,43 @@ namespace PocketGoogle
                 foreach(var getWord in words[id])
                 {
                     if (getWord == word)
-                    {
-                        if (!result.Contains(id))
+                     {
+                            //Console.WriteLine("Trying add id {0}", id);
                             result.Add(id);
-                    }
+                            break;
+                     }
                 }
             }
-            //Console.WriteLine("The end of GetIds");
             return result;
         }
 
         public List<int> GetPositions(int id, string word)
         {
-            //Console.WriteLine("GetPositions");
+            //Console.WriteLine("GetPos");
             var result = new List<int>();
             int count = 0;
-            Id = id;
-            foreach (var getWord in words[Id])
+            foreach (var getWord in words[id])
             {
-                if (getWord.Equals(word))
-                {
-                    //if (!result.Contains(count))
-                        result.Add(count);
-                }
                 if (getWord == "") count++;
-                else count = count + getWord.Length + 1;
-            }
-
-            /*string allWords = String.Join(" ", words[Id]);
-            var length = word.Length;
-
-            while (allWords.Length > 0)
-            {
-                int index = allWords.IndexOf(word);
-                if (index >= 0)
+                else
                 {
-                    count += index;
-                    if (!result.Contains(count))
-                        result.Add(count);
-                    allWords = allWords.Remove(0, index + 1);
-                    count+=1;
+                    if (getWord[0] == word[0])
+                    {
+                        if (getWord.Equals(word))
+                        {
+                            //Console.WriteLine("Trying add id {0}", id);
+                            result.Add(count);
+                        }
+                    }
+                    count = count + getWord.Length + 1;
                 }
-                else break;
-            }*/
-
+            }
             return result;
         }
 
         public void Remove(int id)
         {
-            //Console.WriteLine("Remove");
-            Id = id;
-            Words = new List<string> { };
-            //Console.WriteLine("The end of Remove");
+            words.Remove(id);
         }
     }
 }

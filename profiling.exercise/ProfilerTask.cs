676f3cb548ef10ae2dc66823c6f44b66d18a794f
Alexey Sarapulov <aesee@mail.ru>
2017-10-29 22:57:35 +0600

    Time counter

--- profiling.exercise/ProfilerTask.cs
+++ profiling.exercise/ProfilerTask.cs
@@ -6,9 +6,28 @@ namespace Profiling
 {
 	public class ProfilerTask : IProfiler
 	{
+        double time1, time2;
+        List<ExperimentResult> result = new List<ExperimentResult>();
+        Stopwatch watch = new Stopwatch();
+
 		public List<ExperimentResult> Measure(IRunner runner, int repetitionsCount)
 		{
-			throw new NotImplementedException();
+            foreach (var size in Constants.FieldCounts)
+            {
+                runner.Call(false, size, 1);
+                watch.Restart();
+                runner.Call(false, size, repetitionsCount);
+                watch.Stop();
+                time1 = (double) watch.ElapsedMilliseconds / repetitionsCount;
+                runner.Call(true, size, 1);
+                watch.Restart();
+                runner.Call(true, size, repetitionsCount);
+                watch.Stop();
+                time2 = (double) watch.ElapsedMilliseconds / repetitionsCount;
+                result.Add(new ExperimentResult(size, time2, time1));
+            }
+
+            return result;
 		}
 	}
 }

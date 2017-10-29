a429aa53ad3ba311640b5f2cf691e51a1316b903
Alexey Sarapulov <aesee@mail.ru>
2017-10-29 23:31:39 +0600

    Dropped task because no time for this

--- profiling.exercise/ChartBuilder.cs
+++ profiling.exercise/ChartBuilder.cs
@@ -6,8 +6,23 @@ using ZedGraph;
 
 namespace Profiling
 {
-	class ChartBuilder
-	{
+    class ChartBuilder : IChartBuilder
+    {
+        public Control Build(List<ExperimentResult> result)
+        {
+            GraphPane pane = ZedGraphControl.grap
+            
+            pane.CurveList.Clear();
 
-	}
+            PointPairList time1 = new PointPairList();
+            PointPairList time2 = new PointPairList();
+            foreach (var experiment in result)
+            {
+                time1.Add(experiment.Size, experiment.StructResult);
+                time2.Add(experiment.Size, experiment.ClassResult);
+            }
+
+            LineItem curve1 = pane.AddCurve("Struct Time", time1);
+        }
+    }
 }

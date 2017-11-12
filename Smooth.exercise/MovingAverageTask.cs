d1f5a9b898c8c85015fa97e22555c6ab36e53112
Alexey Sarapulov <aesee@mail.ru>
2017-11-13 00:22:30 +0600

    Moving Average

--- Smooth.exercise/MovingAverageTask.cs
+++ Smooth.exercise/MovingAverageTask.cs
@@ -6,8 +6,36 @@ namespace yield
 	{
 		public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 		{
-			//Fix me!
-			return data;
+            bool isFirstItem = true;
+            double pastAverage = 0, minAverage = 0;
+            int n = 1;
+
+            //foreach(var element in data) n++;
+
+            foreach(var item in data)
+            {
+                item.AvgSmoothedY = (pastAverage * (n - 1) + item.OriginalY) / n;
+                if (n < windowWidth)
+                {
+                    pastAverage = item.AvgSmoothedY;
+                    n++;
+                }
+                else
+                {
+                    pastAverage = item.OriginalY;
+                }
+
+                /*if (isFirstItem)
+                {
+                    item.AvgSmoothedY = item.OriginalY;
+                    isFirstItem = false;
+                    minAverage = item.OriginalY;
+                }
+                else item.AvgSmoothedY = pastAverage - (minAverage / n) + (item.OriginalY / n);
+                pastAverage = item.AvgSmoothedY;*/
+
+                yield return item;
+            }
 		}
 	}
 }

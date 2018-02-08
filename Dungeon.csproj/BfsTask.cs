d115f92d300ca597f503bf620f475382c8036821
Alexey Sarapulov <aesee@mail.ru>
2018-02-08 21:46:45 +0600

    Good perfomance was achieved

--- Dungeon.csproj/BfsTask.cs
+++ Dungeon.csproj/BfsTask.cs
@@ -10,35 +10,34 @@ namespace Dungeon
     public class BfsTask
     {
         private static Queue<SinglyLinkedList<Point>> queue;
-        private static List<Point> visited;
+        private static HashSet<Point> visited;
+        private static Tuple<int, int>[] directions;
 
         public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
         {
             queue = new Queue<SinglyLinkedList<Point>>();
-            visited = new List<Point> { start };
+            visited = new HashSet<Point> { start };
             queue.Enqueue(new SinglyLinkedList<Point>(start, null));
+            directions = new[] { Tuple.Create(-1, 0), Tuple.Create(1, 0), Tuple.Create(0, -1), Tuple.Create(0, 1) };
             var width = map.Dungeon.GetLength(0);
             var height = map.Dungeon.GetLength(1);
-            //var path = new SinglyLinkedList<Point>(start, null);
-            //SinglyLinkedList<Point> path = null;
-
+        
             while (queue.Count != 0)
             {
                 var point = queue.Dequeue();
-                if (point.Value.X < 0 || point.Value.Y < 0 || point.Value.X >= width || point.Value.Y >= height) continue;
+                if (point.Value.X < 0 || point.Value.Y < 0 ||
+                    point.Value.X >= width || point.Value.Y >= height) continue;
                 if (map.Dungeon[point.Value.X, point.Value.Y] == MapCell.Wall) continue;
 
                 if (chests.Contains(point.Value)) yield return point;
 
-                for (var dx = -1; dx <= 1; dx++)
-                    for (var dy = -1; dy <= 1; dy++)
-                        if (Math.Abs(dx)!=Math.Abs(dy))
-                            AddToQueue(new Point(point.Value.X + dx, point.Value.Y + dy), point);
+                foreach (var dir in directions)
+                            AddToQueue(new Point(point.Value.X + dir.Item1, point.Value.Y + dir.Item2), point);
             }
 
             yield break;
         }
-
+        
         private static void AddToQueue(Point point, SinglyLinkedList<Point> prevPoint)
         {
             if (!visited.Contains(point))

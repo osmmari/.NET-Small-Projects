5a5627b8fc9958819fff77fc02a833b84fc136c4
Alexey Sarapulov <aesee@mail.ru>
2018-06-13 01:15:03 +0600

    Completed Data Structure

---
+++ Inheritance.DataStructure.csproj/Category.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.DataStructure
+{
+    public class Category : IComparable
+    {
+        private string name;
+        private MessageType type;
+        private MessageTopic topic;
+
+        public Category(string name, MessageType type, MessageTopic topic)
+        {
+            this.name = name;
+            this.type = type;
+            this.topic = topic;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (this == null || obj == null) return 1;
+            Category category2 = obj as Category;
+            //Console.WriteLine(this.ToString() + category2.ToString());
+            if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(category2.name))
+                return 1;
+            else
+            {
+                for (int i = 0; i < Math.Min(name.Length, category2.name.Length); i++)
+                {
+                    if (name[i] > category2.name[i]) return 1;
+                    else if (name[i] < category2.name[i]) return -1;
+                }
+            }
+
+            if (type > category2.type) return 1;
+            else if (type < category2.type) return -1;
+
+            if (topic > category2.topic) return 1;
+            else if (topic < category2.topic) return -1;
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}.{1}.{2}", name, type.ToString(), topic.ToString());
+        }
+
+        public bool Equals(Category obj)
+        {
+            return this.CompareTo(obj) == 0 ? true : false;
+        }
+
+        public static bool operator <=(Category element1, Category element2)
+        {
+            return element1.CompareTo(element2) <= 0 ? true : false;
+        }
+
+        public static bool operator >=(Category element1, Category element2)
+        {
+            return element1.CompareTo(element2) >= 0 ? true : false;
+        }
+
+        public static bool operator <(Category element1, Category element2)
+        {
+            return element1.CompareTo(element2) < 0 ? true : false;
+        }
+
+        public static bool operator >(Category element1, Category element2)
+        {
+            return element1.CompareTo(element2) > 0 ? true : false;
+        }
+    }
+}
---
+++ Inheritance.DataStructure.csproj/Category_should.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ReSharper disable IsExpressionAlwaysTrue
+
+namespace Inheritance.DataStructure
+{
+    [TestFixture]
+    public class Category_should
+    {
+        Category A11 = new Category("A", MessageType.Incoming, MessageTopic.Subscribe);
+        Category A21 = new Category("A", MessageType.Outgoing, MessageTopic.Subscribe);
+        Category A12 = new Category("A", MessageType.Incoming, MessageTopic.Error);
+        Category B11 = new Category("B", MessageType.Incoming, MessageTopic.Subscribe);
+        Category C1 = new Category(null, MessageType.Incoming, MessageTopic.Subscribe);
+        Category C2 = new Category(null, MessageType.Incoming, MessageTopic.Subscribe);
+
+        Category[] Descending()
+        {
+            return new[] { A11, A12, A21, B11 };
+        }
+
+        Category[] SpecialCase()
+        {
+            return new[] { A11, A12, A21, B11, C1, C2 };
+        }
+
+        Category A11_copy = new Category("A", MessageType.Incoming, MessageTopic.Subscribe);
+
+        [Test]
+        public void NullReference()
+        {
+            var a = SpecialCase();
+            Assert.False(A11.Equals(C1));
+            Assert.False(A11.Equals(C2));
+            Assert.False(C1.Equals(A11));
+            Assert.False(C2.Equals(A11));
+            //Assert.True(C1.Equals(C2));
+        }
+
+        [Test]
+        public void ImplementEqualsCorrectly()
+        {
+            var a = Descending();
+            for (int i = 0; i < a.Length; i++)
+                for (int j = 0; j < a.Length; j++)
+                    Assert.AreEqual(i == j, a[i].Equals(a[j]), $"Error on {i} {j}");
+            Assert.True(A11.Equals(A11_copy));
+        }
+
+        [Test]
+        public void ImplementCompareToCorrectly()
+        {
+            var a = Descending();
+            for (int i = 0; i < a.Length; i++)
+                for (int j = 0; j < a.Length; j++)
+                    Assert.AreEqual(Math.Sign(i.CompareTo(j)), Math.Sign(a[i].CompareTo(a[j])), $"Error on {i} {j}");
+            Assert.AreEqual(0, A11.CompareTo(A11_copy));
+        }
+
+        [Test]
+        public void ImplementOperatorsCorrectly()
+        {
+            var a = Descending();
+            for (int i = 0; i < a.Length; i++)
+                for (int j = 0; j < a.Length; j++)
+                {
+                    Assert.AreEqual(i <= j, a[i] <= a[j], $"Error on <=, {i} {j}");
+                    Assert.AreEqual(i >= j, a[i] >= a[j], $"Error on >=, {i} {j}");
+                    Assert.AreEqual(i < j, a[i] < a[j], $"Error on <, {i} {j}");
+                    Assert.AreEqual(i > j, a[i] > a[j], $"Error on >, {i} {j}");
+                    Assert.AreEqual(i == j, a[i] == a[j], $"Error on ==, {i} {j}");
+                    Assert.AreEqual(i != j, a[i] != a[j], $"Error on !=, {i} {j}");
+                }
+        }
+
+        [Test]
+        public void ImplementIComparableInterface()
+        {
+            Assert.True(A11 is IComparable);
+        }
+
+        [Test]
+        public void ImplementToStringCorrectly()
+        {
+            Assert.AreEqual("A.Incoming.Subscribe", A11.ToString());
+        }
+    }
+}

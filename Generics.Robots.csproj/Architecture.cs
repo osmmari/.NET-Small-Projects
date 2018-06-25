f3b4fa30bf638b3d7e00fe78803f083e5d0c22ba
Alexey Sarapulov <aesee@mail.ru>
2018-06-25 22:14:23 +0600

    Do Table Task

---
+++ Generics.Robots.csproj/Architecture.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Generics.Robots
+{
+    public abstract class RobotAI
+    {
+        public abstract object GetCommand();
+    }
+
+    public class ShooterAI : RobotAI
+    {
+        int counter = 1;
+
+        public override object GetCommand()
+        {
+            return ShooterCommand.ForCounter(counter++);
+        }
+    }
+
+    public class BuilderAI : RobotAI
+    {
+        int counter = 1;
+        public override object GetCommand()
+        {
+            return BuilderCommand.ForCounter(counter++);
+        }
+    }
+
+    public abstract class Device
+    {
+        public abstract string ExecuteCommand(object command);
+    }
+
+    public class Mover : Device
+    {
+        public override string ExecuteCommand(object _command)
+        {
+            var command = _command as IMoveCommand;
+            if (command == null)
+                throw new ArgumentException();
+            return $"MOV {command.Destination.X}, {command.Destination.Y}";
+        }
+    }
+
+
+
+    public class Robot
+    {
+        RobotAI ai;
+        Device device;
+
+        public Robot(RobotAI ai, Device executor)
+        {
+            this.ai = ai;
+            this.device = executor;
+        }
+
+        public IEnumerable<string> Start(int steps)
+        {
+             for (int i=0;i<steps;i++)
+             {
+                 var command = ai.GetCommand();
+                 if (command == null)
+                     break;
+                 yield return device.ExecuteCommand(command);
+             }
+
+        }
+
+        public static Robot Create(RobotAI ai, Device executor)
+        {
+            return new Robot(ai, executor);
+        }
+    }
+    
+
+}
---
+++ Generics.Tables.csproj/Table.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics.Tables
+{
+	public class Table<TRow, TColumn, TValue> where TValue : new()
+	{
+		private HashSet<TRow> rows;
+		private HashSet<TColumn> columns;
+
+		public HashSet<TRow> Rows { get { return rows; } set { rows = value; } }
+		public HashSet<TColumn> Columns { get { return columns; } set { columns = value; } }
+
+		private Dictionary<Tuple<TRow, TColumn>, TValue> table;
+
+		public NewCell Open { get { return new NewCell (this); } set { } }
+		public ExistedCell Existed { get { return new ExistedCell (this); } set {} }
+
+		public TValue GetValue(TRow row, TColumn column)
+		{
+			var index = new Tuple<TRow, TColumn> (row, column);
+			return table.ContainsKey (index) ? table [index] : new TValue ();
+		}
+
+        public void SetValue(TRow row, TColumn column, TValue value)
+        {
+            var index = new Tuple<TRow, TColumn>(row, column);
+            table[index] = value;
+        }
+
+		public void AddRow(TRow row)
+		{
+			if (!rows.Contains (row))
+				rows.Add (row);
+		}
+
+		public void AddColumn(TColumn column)
+		{
+			if (!columns.Contains (column))
+				columns.Add (column);
+		}
+
+		public Table()
+		{
+			rows = new HashSet<TRow> ();
+			columns = new HashSet<TColumn> ();
+			table = new Dictionary<Tuple<TRow, TColumn>, TValue> ();
+		}
+
+		public abstract class Cell
+		{
+			protected Table<TRow, TColumn, TValue> table;
+
+			public Cell(Table<TRow, TColumn, TValue> _table)
+			{
+				table = _table;
+			}
+		}
+
+		public class NewCell : Cell
+		{
+			public NewCell(Table<TRow, TColumn, TValue> _table) : base(_table) {}
+
+			public TValue this[TRow row, TColumn column]
+			{
+				get 
+				{
+                    return table.GetValue(row, column);
+				}
+				set
+                {
+                    table.AddRow(row);
+                    table.AddColumn(column);
+                    table.SetValue(row, column, value);
+                }
+			}
+		}
+
+		public class ExistedCell : Cell
+		{
+			public ExistedCell(Table<TRow, TColumn, TValue> _table) : base(_table) {}
+
+			public TValue this[TRow row, TColumn column]
+			{
+                get
+                {
+                    if (table.Rows.Contains(row) && table.Columns.Contains(column))
+                        return table.GetValue(row, column);
+                    else
+                    {
+                        throw new ArgumentException("There's no such cell.");
+                    }
+                }
+                set
+                {
+                    if (table.Rows.Contains(row) && table.Columns.Contains(column))
+                        table.SetValue(row, column, value);
+                    else throw new ArgumentException("There's no such cell.");
+                }
+            }
+		}
+	}
+}

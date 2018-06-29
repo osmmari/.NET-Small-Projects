84caa642b4ca3c40081bf86e113665c52f3fac46
Alexey Sarapulov <aesee@mail.ru>
2018-06-29 18:12:31 +0600

    Do Tree traversal task

---
+++ Delegates.PairsAnalysis.csproj/Analysis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.PairsAnalysis
+{
+    public static class Analysis
+    {
+        public static int FindMaxPeriodIndex(params DateTime[] data)
+        {
+            return new MaxPauseFinder().Analyze(data);
+        }
+
+        public static double FindAverageRelativeDifference(params double[] data)
+        {
+            return new AverageDifferenceFinder().Analyze(data);
+        }
+    }
+}
--- Delegates.TreeTraversal.csproj/Traversal.cs
+++ Delegates.TreeTraversal.csproj/Traversal.cs
@@ -6,5 +6,74 @@ using System.Threading.Tasks;
 
 namespace Delegates.TreeTraversal
 {
-   
+    public static class Traversal
+    {
+        private static IEnumerable<T> Traverse<T, Data>(Data data, Func<Data, IEnumerable<T>> function)
+        {
+            var result = function(data);
+            return result;
+        }
+
+        private static IEnumerable<int> TraverseBinaryTree(BinaryTree<int> data)
+        {
+            if (data != null)
+            {
+                yield return data.Value;
+                if (data.Left != null)
+                {
+                    foreach (var item in TraverseBinaryTree(data.Left))
+                    yield return item;
+                }
+                if (data.Right != null)
+                {
+                    foreach (var item in TraverseBinaryTree(data.Right))
+                        yield return item;
+                }
+            }
+        }
+
+        private static IEnumerable<Job> TraverseJobs(Job data)
+        {
+            if (data != null)
+            {
+                if (data.Subjobs != null && data.Subjobs.Count > 0)
+                {
+                    foreach (var job in data.Subjobs)
+                        foreach (var item in TraverseJobs(job))
+                            yield return item;
+                }
+                else
+                    yield return data;
+            }
+        }
+
+        private static IEnumerable<Product> TraverseProducts(ProductCategory data)
+        {
+			foreach (var category in data.Categories)
+			{
+				foreach (var item in TraverseProducts (category))
+					yield return item;
+			}
+			foreach (var product in data.Products)
+			{
+				yield return product;
+			}
+        }
+        //---------------------------------------------------------------//
+
+        public static IEnumerable<int> GetBinaryTreeValues(BinaryTree<int> data)
+        {
+            return Traverse(data, TraverseBinaryTree);
+        }
+
+        public static IEnumerable<Job> GetEndJobs(Job data)
+        {
+            return Traverse(data, TraverseJobs);
+        }
+
+        public static IEnumerable<Product> GetProducts(ProductCategory data)
+        {
+            return Traverse(data, TraverseProducts);
+        }
+    }
 }

6b36ec31e206126c80ea375a8d92bb3075ceb162
Alexey Sarapulov <aesee@mail.ru>
2018-06-29 18:41:37 +0600

    Do finding pairs with delegates and linq

--- Delegates.PairsAnalysis.csproj/Analysis.cs
+++ Delegates.PairsAnalysis.csproj/Analysis.cs
@@ -10,12 +10,73 @@ namespace Delegates.PairsAnalysis
     {
         public static int FindMaxPeriodIndex(params DateTime[] data)
         {
-            return new MaxPauseFinder().Analyze(data);
+			return Finder(data, MaxIndexProcess).MaxIndex();
         }
 
         public static double FindAverageRelativeDifference(params double[] data)
         {
-            return new AverageDifferenceFinder().Analyze(data);
+			return Finder(data, AverageProcess).AverageDifference();
         }
+
+		public static IEnumerable<double> Finder<T>(IEnumerable<T> data, Func<T,T,double> process)
+		{
+			return data.Pairs ().Select(pair => process(pair.Item1, pair.Item2));
+		}
+
+		private static double MaxIndexProcess(DateTime source1, DateTime source2)
+		{
+			return (source2 - source1).TotalSeconds;
+		}
+
+		private static double AverageProcess(double source1, double source2)
+		{
+			return (source2 - source1) / source1;
+		}
     }
+
+	public static class AnalysisExtension
+	{
+		public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> data)
+		{
+			//if (data.Count() < 2) throw new ArgumentException();
+
+			T previous = default(T);
+			using (var it = data.GetEnumerator ())
+			{
+				if (it.MoveNext ())
+					previous = it.Current;
+
+				while (it.MoveNext ())
+				{
+					yield return new Tuple<T,T> (previous, it.Current);
+					previous = it.Current;
+				}
+			}
+		}
+
+		public static int MaxIndex<T>(this IEnumerable<T> data) where T : IComparable<T>
+		{
+            //return data.ToList ().IndexOf (data.Max ());
+            int maxIndex = -1;
+            T maxValue = default(T);
+
+            int index = 0;
+            foreach (T value in data)
+            {
+                if (value.CompareTo(maxValue) > 0 || maxIndex == -1)
+                {
+                    maxIndex = index;
+                    maxValue = value;
+                }
+                index++;
+            }
+            if (maxIndex < 0) throw new ArgumentException();
+            return maxIndex;
+        }
+
+		public static double AverageDifference(this IEnumerable<double> differences)
+		{
+			return differences.Sum() / differences.Count ();
+		}
+	}
 }
--- image.exercise/SobelFilterTask.cs
+++ image.exercise/SobelFilterTask.cs
@@ -4,38 +4,47 @@ namespace Recognizer
 {
     internal static class SobelFilterTask
     {
-        /* 
-		Разберитесь, как работает нижеследующий код (называемый фильтрацией Собеля), 
-		и какое отношение к нему имеют эти матрицы:
-		
-		     | -1 -2 -1 |           | -1  0  1 |
-		Sx = |  0  0  0 |      Sy = | -2  0  2 |
-		     |  1  2  1 |           | -1  0  1 |
-		
-		https://ru.wikipedia.org/wiki/%D0%9E%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80_%D0%A1%D0%BE%D0%B1%D0%B5%D0%BB%D1%8F
-		
-		Попробуйте заменить фильтр Собеля 3x3 на фильтр Собеля 5x5 и сравните результаты. 
-		http://www.cim.mcgill.ca/~image529/TA529/Image529_99/assignments/edge_detection/references/sobel.htm
-
-		Обобщите код применения фильтра так, чтобы можно было передавать ему любые матрицы, любого нечетного размера.
-		Фильтры Собеля размеров 3 и 5 должны быть частным случаем. 
-		После такого обобщения менять фильтр Собеля одного размера на другой будет легко.
-		*/
-
         public static double[,] SobelFilter(double[,] g, double[,] sx)
         {
             var width = g.GetLength(0);
             var height = g.GetLength(1);
             var result = new double[width, height];
-            for (int x = 1; x < width - 1; x++)
+            
+			var sy = Transpose (sx);
+
+			for (int x = 1; x < width - 1; x++)
                 for (int y = 1; y < height - 1; y++)
-                {
-                    // Вместо этого кода должно быть умножение матриц sx и полученной из неё sy на окрестность точки (x, y)
-                    var gx = -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1] + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                    var gy = -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1] + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
-                    result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+                {                    
+					var gx = MultiplyMatrixes (g, sx, x, y);
+					var gy = MultiplyMatrixes (g, sy, x, y);
+                    
+					result[x, y] = Math.Sqrt(gx * gx + gy * gy);
                 }
             return result;
         }
+
+        private static double MultiplyMatrixes(double[,] image, double[,] filter, int pixelX, int pixelY)
+        {
+            double result = 0;
+            var length = filter.GetLength(0);
+            for (var x = 0; x < length; x++)
+                for (var y = 0; y < length; y++)
+                {
+                    result += image[pixelX - 1 + x, pixelY - 1 + y] * filter[x, y];
+                }
+            return result;
+        }
+
+        private static double[,] Transpose(double[,] sx)
+		{
+			var length = sx.GetLength (0);
+			var sy = new double[length,length];
+			for (var x = 0; x < length; x++)
+				for (var y = 0; y < length; y++)
+				{
+					sy [x, y] = sx [y, x];
+				}
+			return sy;
+		}
     }
 }

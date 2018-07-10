8e76db69af9369de7a0d245a6183b5a7bc7ac44f
Alexey Sarapulov <aesee@mail.ru>
2018-07-11 00:20:08 +0600

    Do dispose pattern

---
+++ Memory.API.csproj/APIObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        int id;
+
+        public APIObject(int id)
+        {
+            this.id = id;
+            MagicAPI.Allocate(id);
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    MagicAPI.Free(id);
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        ~APIObject()
+        {        
+           Dispose(true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
